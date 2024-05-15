77044cd40212cd90c86b44f9b45abff748ccea43
Muaz Ahmad <123929626+oyemauz@users.noreply.github.com>
2024-05-15 22:32:02 +0500

    modified login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
